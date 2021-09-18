64044b2168e4c486a66eca6edae7c2ed1f30e5ef
dwinovic <nd.cahya19@gmail.com>
2021-09-18 15:28:27 +0700

    fixing bug in profile update

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['roomActiveReducer'],
+  blacklist: ['roomActiveReducer', 'loadingReducer'],
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
 
